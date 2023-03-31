eb3c761c46fdea3fd3672e27a1fcd7111bdb71c6
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-03-31 17:07:58 -0300

    feat: add Movies CRUD

---
+++ RazorPagesMovies/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovies.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime DateReleased { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
