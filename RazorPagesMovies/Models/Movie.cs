f8bc496aea5a72cd8a02a4c128257f1d24da972d
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-01 14:53:27 -0300

    feat: add validation database

--- RazorPagesMovies/Models/Movie.cs
+++ RazorPagesMovies/Models/Movie.cs
@@ -5,16 +5,26 @@ namespace RazorPagesMovies.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "Título")]
         public string Title { get; set; } = string.Empty;
 
         [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime DateReleased { get; set; }
+
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$]")]
+        [Required(ErrorMessage = "Escreva o Gênero do Filme")]
+        [MaxLength(100, ErrorMessage = "Nome muito extenso.")]
         [Display(Name = "Gênero")]
         public string Gender { get; set; } = string.Empty;
+
+        [DataType(DataType.Currency)]
         [Display(Name = "Preço")]
         public decimal Price { get; set; }
+
+        [Range(0, 10)]
         [Display(Name = "Nota de Avaliação")]
         public int Avaliation { get; set; } = 0;
     }
