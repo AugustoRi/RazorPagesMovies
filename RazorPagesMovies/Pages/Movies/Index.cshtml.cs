53b8f9d3d508b29df6e2ce6e1335935649f2f37b
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-04-01 14:07:49 -0300

    feat: add movie search input

--- RazorPagesMovies/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovies/Pages/Movies/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovies.Data;
@@ -21,11 +22,26 @@ namespace RazorPagesMovies.Pages.Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchInputContent { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string GenderMovie { get; set; }
+
+        public SelectList Genders { get; set; }
+
         public async Task OnGetAsync()
         {
             if (_context.Movie != null)
             {
-                Movie = await _context.Movie.ToListAsync();
+                var movies = from m in _context.Movie
+                            select m;
+                if (!string.IsNullOrWhiteSpace(SearchInputContent))
+                {
+                    movies = movies.Where(f => f.Title.Contains(SearchInputContent));
+                }
+
+                Movie = await movies.ToListAsync();
             }
         }
     }
