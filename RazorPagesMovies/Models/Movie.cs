f9924c067dde5a55f7b4d8d30ad774fc824bacf7
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2023-03-31 21:56:06 -0300

    style: edit names in html movies

--- RazorPagesMovies/Models/Movie.cs
+++ RazorPagesMovies/Models/Movie.cs
@@ -5,11 +5,15 @@ namespace RazorPagesMovies.Models
     public class Movie
     {
         public int ID { get; set; }
+        [Display(Name = "Título")]
         public string Title { get; set; } = string.Empty;
 
+        [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime DateReleased { get; set; }
+        [Display(Name = "Gênero")]
         public string Gender { get; set; } = string.Empty;
+        [Display(Name = "Preço")]
         public decimal Price { get; set; }
     }
 }
